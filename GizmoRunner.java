8a5aa8295e5ba8a5fb5ffafa9bc3a3fd6a0b3a27
Debarshi <K12220794@students.katyisd.org>
2023-11-06 21:50:58 -0600

    asdgewgasdgasdgasdg

---
+++ GizmoRunner.java
@@ -0,0 +1,11 @@
+
+
+public class GizmoRunner {
+    //just temporary to test the menu in the future we'll have logic to switch between panels in here
+    public static void main (String[] args) {
+
+        MainMenu graphicWindow = new MainMenu("Main Menu");
+
+    }
+    
+}
--- MainMenu.java
+++ MainMenu.java
@@ -1,11 +1,54 @@
-public class MainMenu {
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
 
-    public static void main (String[] args) {
+import javax.swing.*;
 
-        System.out.println("Hello");
+public class MainMenu extends JFrame{
+    JButton start, settings, exit;
+    JLabel background;
+    private static final int WIDTH = 1200;
+    private static final int HEIGHT = 630;
+    public MainMenu(String framename) {
+        //sets the background of the menu and inits it and also adds jbuttons for each option
+        super(framename);
+        Icon temp = new ImageIcon("Start.png");
+        start = new JButton(temp);
+        temp = new ImageIcon("Settings.png");
+        settings = new JButton(temp);
+        temp = new ImageIcon("Exit.png");
+        exit = new JButton(temp);
+        setLayout(null);
+        background = new JLabel(new ImageIcon("back.png"), JLabel.CENTER);
+        background.setBounds(0, 0, 1200, 630);
+        add(background);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+   
+    public void actionPerformed(ActionEvent e) {
+        //we will change the prints to actual functions once other classes are made
+        if (e.getSource() == start) {
+            
+            System.out.println("Start");
+
+        } else if (e.getSource() == settings) {
+
+            System.out.println("Open Settings");
+
+        } else {
+
+            System.out.println("Terminate the game");
+
+        }
 
     }
-        
+    
+
+
 
 
 }
