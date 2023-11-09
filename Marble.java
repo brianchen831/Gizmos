90992e31226dcbd87dbd8025c4b013e2ca6433bd
Debarshi <K12220794@students.katyisd.org>
2023-11-09 17:34:15 -0600

    marble class stuff

--- Marble.java
+++ Marble.java
@@ -1,10 +1,25 @@
+import java.io.*;
+import java.awt.image.*;
+import java.util.*;
 
 public class Marble {
+
+	private String marbleColor;
+	private BufferedImage redMarble, blueMarble, yellowMarble, greyMarble;
+
 	public Marble() {
 		
-		
+		//set all the images
 		
 	}
 	
-
+	public void setRandColor() {
+		double r = Math.floor(Math.random() * 3);
+		if (r==0) { marbleColor = "Blue"; }
+		if (r==1) { marbleColor = "Red"; }
+		if (r==2) { marbleColor = "Grey"; }
+		if (r==3) { marbleColor = "Yellow"; }
+	}
+	
 }
+ 
