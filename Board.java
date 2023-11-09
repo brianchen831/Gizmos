9d8ea0b419300a43dcaa09bc5f95238c8d366ab7
debarshi13 <112979844+debarshi13@users.noreply.github.com>
2023-11-09 13:40:38 -0600

    Start
    Start

--- Board.java
+++ Board.java
@@ -1,1 +1,51 @@
+import java.awt.image.*;
+import java.io.*;
+import java.util.*;
 
+public class Board {
+	
+	private BufferedImage marbleDispenser;
+	private HashMap<Integer, ArrayList<Gizmo>> playableGizmos;
+	private ArrayList<Marble> visibleMarbles;
+	public Board() {
+		
+		
+		
+		
+		
+	}
+	
+	public void resetMarbleDisp() {
+		
+		
+		
+	}
+	
+	public void resetGizmos() {
+		
+		
+		
+		
+		
+	}
+	
+	
+	public void refreshBoard() {
+		
+		
+		
+		
+		
+	}
+	
+	
+	public void drawBoard() {
+		
+		
+		
+		
+		
+	}
+	
+	
+}
---
+++ Gizmo.java
@@ -0,0 +1,10 @@
+
+public class Gizmo {
+	
+	public Gizmo() {
+		
+		
+	}
+	
+
+}
