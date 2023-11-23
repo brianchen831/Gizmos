218d960532a83703f058e3c3fba1fe4abb612636
Brian Chen <brianyichen@gmail.com>
2023-11-22 20:33:31 -0600

    awdawd

--- Board.java
+++ Board.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 public class Board extends JFrame{
 
 
-
+//test
 	private static final int WIDTH = 1920;
 	private static final int HEIGHT = 1080;
 	public Board (String framename) {
